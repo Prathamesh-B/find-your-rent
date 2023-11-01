48e1d8c55687ac562c3a18d8a6185b55ada38919
Riya Torgal <riyatorgal@gmail.com>
2023-11-01 23:00:48 +0530

    Add Footer

---
+++ app/components/Footer/index.ts
@@ -0,0 +1,19 @@
+export const footerLinks = [
+    {
+        title: "About",
+        links: [
+            { title: "How it works", url: "/" },
+            { title: "Featured", url: "/" },
+            { title: "Support", url: "/" },
+        ],
+    },
+    
+    {
+        title: "Socials",
+        links: [
+            { title: "Discord", url: "/" },
+            { title: "Instagram", url: "/" },
+            { title: "Twitter", url: "/" },
+        ],
+    },
+];
