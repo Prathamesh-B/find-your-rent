f9068c9e728dd5fde2330432d9b05285f780eeb3
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2023-11-04 10:42:27 +0530

    Add ItemCard
    Co-authored-by: Riya Torgal <RiyaTorgal@users.noreply.github.com>

---
+++ app/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { Card, Image, Text, Badge, Button, Group, Grid } from "@mantine/core";
+import Link from "next/link";
+
+const ItemCards = () => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://png.pngtree.com/png-vector/20230318/ourmid/pngtree-the-books-clipart-vector-png-image_6653533.png"
+          height={50}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Grid mt="md">
+        <Grid.Col span={6}>
+          <Text justify="left" align="left" fw={500}>
+            Item Name
+          </Text>
+        </Grid.Col>
+        <div className="col-span-2"></div>
+
+        <Grid.Col span={6}>
+          <Group justify="right" align="right">
+            <p className="flex text-[32px]">
+              <span className="self-start text-[14px] ">Rent: Rs</span>
+              0.0
+              <span className="self-end text-[14px] ">/day</span>
+            </p>
+          </Group>
+        </Grid.Col>
+      </Grid>
+
+      <Text size="sm" c="dimmed" mt="xs" mb="xs">
+        Description of the Item(in Brief)
+      </Text>
+
+      <Text fw={400}>Owner Name</Text>
+
+      <Link href="/product">
+        <Button
+          variant="light"
+          color="rgba(238, 147, 34, 1)"
+          fullWidth
+          mt="md"
+          radius="md"
+        >
+          View More
+        </Button>
+      </Link>
+    </Card>
+  );
+};
+
+export default ItemCards;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
 import Hero from './components/Hero/Hero'
-
+import ItemCards from './components/ItemCard/ItemCard'
+import { Button } from '@mantine/core';
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+      <div className='grid 2xl:grid-cols-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-10 px-7'>
+        <ItemCards />
+        <ItemCards />
+        <ItemCards />
+        <ItemCards />
+        <ItemCards />
+      </div>
+      <div className='flex-center mt-8'>
+        <Button variant="filled" size="md" color="rgba(238, 147, 34, 1)">Show More</Button>
+      </div>
     </main>
   )
 }
