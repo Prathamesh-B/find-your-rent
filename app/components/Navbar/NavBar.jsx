6a73e7cd31d26da87ffc1f2ef57189074521214c
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2023-11-05 18:00:03 +0530

    Add Menu

--- app/components/Navbar/NavBar.jsx
+++ app/components/Navbar/NavBar.jsx
@@ -3,9 +3,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Avatar, Menu, rem } from "@mantine/core";
 import { useRouter, useSearchParams } from "next/navigation";
 import { showNotification } from "@mantine/notifications";
-import { BiLogOut } from "react-icons/bi";
+import { BiLogOut, BiBookAdd } from "react-icons/bi";
 
 const Navbar = () => {
   const router = useRouter();
@@ -38,22 +39,45 @@ const Navbar = () => {
         </Link>
 
         {token ? (
-          <button
-            onClick={() => {
-              handleLogout();
-              showNotification({
-                id: "logout",
-                autoClose: true,
-                color: "indigo",
-                icon: <BiLogOut />,
-                title: "Logging out",
-                message: "Logged out Successfully",
-              });
-            }}
-            className="px-2 py-2 text-sm leading-5 font-bold text-white transition-colors duration-200 transform bg-orange-fyr rounded hover:bg-oragne-secondary-fyr focus:outline-none"
-          >
-            Log out
-          </button>
+          <>
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <Avatar radius="xl" />
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Label>Application</Menu.Label>
+                <Menu.Item
+                  onClick={() => {
+                    router.push("/product/add");
+                  }}
+                  leftSection={
+                    <BiBookAdd style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Add Item
+                </Menu.Item>
+                <Menu.Item
+                  className="hover:bg-red-100"
+                  onClick={() => {
+                    handleLogout();
+                    showNotification({
+                      id: "logout",
+                      autoClose: true,
+                      color: "indigo",
+                      icon: <BiLogOut />,
+                      title: "Logging out",
+                      message: "Logged out Successfully",
+                    });
+                  }}
+                  leftSection={
+                    <BiLogOut style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Log out
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </>
         ) : (
           <Link href="/login" className="flex justify-center items-center">
             <button className="px-2 py-2 text-sm leading-5 font-bold text-white transition-colors duration-200 transform bg-orange-fyr rounded hover:bg-oragne-secondary-fyr focus:outline-none">
