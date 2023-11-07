be191a4b53e32f32226ab3dbd52fb364e77e687d
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2023-11-07 19:32:44 +0530

    Add update and delete api

---
+++ app/api/item/delete/route.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+import { NextResponse } from 'next/server';
+import prisma from '@/app/lib/db'
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        const { itemID, authToken } = body;
+        jwt.verify(authToken, process.env.NEXT_PUBLIC_JWT_SECRET);
+        const deletedItem = await prisma.item.delete({
+            where: {
+                id: parseInt(itemID),
+            }
+        })
+        return NextResponse.json({ success: true, deletedItem, message: "Item Deleted" }, { status: 200 })
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError){
+            return NextResponse.json({ success: false, message: "JsonWebTokenError: invalid signature!" }, { status: 401 })
+        }
+        console.log(error)
+        return NextResponse.json({ success: false, message: "Something went wrong!" }, { status: 500 })
+    } finally {
+        await prisma.$disconnect();
+    }
+}
---
+++ app/api/item/update/route.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+import { NextResponse } from 'next/server';
+import prisma from '@/app/lib/db'
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        const { id, title, description, price, photos, authToken } = body;
+        jwt.verify(authToken, process.env.NEXT_PUBLIC_JWT_SECRET);
+        const updatedItem = await prisma.item.update({
+            where: {
+                id: parseInt(id),
+            },
+            data: {
+                title: title,
+                description: description,
+                price: price,
+                photos: photos
+            },
+        })
+        return NextResponse.json({ success: true, updatedItem, message: "Item Updated" }, { status: 200 })
+    } catch (error) {
+        console.log(error)
+        if (error instanceof jwt.JsonWebTokenError) {
+            return NextResponse.json({ success: false, message: "JsonWebTokenError: invalid signature!" }, { status: 401 })
+        }
+        console.log(error)
+        return NextResponse.json({ success: false, message: "Something went wrong!" }, { status: 500 })
+    } finally {
+        await prisma.$disconnect();
+    }
+}
