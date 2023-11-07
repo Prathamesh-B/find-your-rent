510726a58a4d191a005b2c09b610a8335ba154bf
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2023-11-07 11:25:04 +0530

    Add userItems api

---
+++ app/api/item/userItems/[number]/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+const jwt = require("jsonwebtoken");
+import prisma from '@/app/lib/db'
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        const { authToken } = body;
+        const UserInfo = jwt.verify(authToken, process.env.NEXT_PUBLIC_JWT_SECRET);
+        const page = req.url.split("userItems/")[1] - 1
+        const result = await prisma.item.findMany({
+            
+            skip: page > 0 ? page * 8 : 0,
+            take: 8,
+            where: {
+                ownerId: UserInfo.id, // Filter items by user ID
+            }
+        });
+        return NextResponse.json({ success: true, result }, { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ success: false, message: "Something went wrong!" }, { status: 500 })
+    }
+}
--- app/userItems/page.js
+++ app/userItems/page.js
@@ -1,19 +1,71 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import ItemCard from "../components/ItemCard/ItemCard";
 
 const userItems = () => {
+    const [items, setItems] = useState([]);
+    const [skip, setSkip] = useState(1);
+    const [hasMoreData, setHasMoreData] = useState(true);
+    const [shouldRunEffect, setShouldRunEffect] = useState(false);
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!shouldRunEffect) {
+            setShouldRunEffect(true);
+            return;
+        }
+        const fetchItems = async () => {
+            const response = await fetch(`/api/item/userItems/${skip}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ authToken: token })
+            });
+            const json = await response.json();
+            if (json.success) {
+                setItems((oldItems) => [...oldItems, ...json.result]);
+                if (json.result.length < 8) {
+                    setHasMoreData(false); // No more data to fetch
+                }
+            }
+        };
+        fetchItems();
+    }, [skip, shouldRunEffect]);
+
+    const loadMore = () => {
+        setSkip((oldSkip) => oldSkip + 1);
+    };
     return (
         <>
-        <div className="pt-8" />
+            <div className="pt-8" />
             <p className="2xl:text-[30px] sm:text-[30px] text-[30px] font-semibold pt-10 px-7">
                 My Items:
             </p>
             <div className="grid 2xl:grid-cols-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-10 px-7">
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
+                {items?.map((item) => (
+                    <div className="col-md-4" key={uuidv4()}>
+                        <ItemCard
+                            id={item.id}
+                            title={item.title}
+                            price={item.price}
+                            description={item.description}
+                            photos={item.photos[0]}
+                        />
+                    </div>
+                ))}
             </div>
+            {hasMoreData && (
+                <div className="flex-center m-4">
+                    <button
+                        onClick={loadMore}
+                        className="mt-4 px-4 py-2 leading-5 font-bold text-white transition-colors duration-200 transform bg-orange-fyr rounded hover:bg-oragne-secondary-fyr focus:outline-none"
+                    >
+                        Show More
+                    </button>
+                </div>
+            )}
         </>
     )
 }
