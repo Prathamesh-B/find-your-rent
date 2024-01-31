780740931c08c920720b9553f7dc32b2c7682d45
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2024-01-31 23:24:38 +0530

    Add API for user to view rental status

--- app/api/item/rentRequests/route.js
+++ app/api/item/rentRequests/route.js
@@ -25,7 +25,36 @@ export async function POST(req) {
         return NextResponse.json({ success: true, rentals }, { status: 200 });
     } catch (error) {
         console.log(error);
-        if (error instanceof jwt.JsonWebTokenError){
+        if (error instanceof jwt.JsonWebTokenError) {
+            return NextResponse.json({ success: false, message: "JsonWebTokenError: invalid signature!" }, { status: 401 });
+        }
+        return NextResponse.json({ success: false, message: "Something went wrong!" }, { status: 500 });
+    }
+}
+
+
+export async function PUT(req) {
+    try {
+        const body = await req.json();
+        const { authToken } = body;
+        const UserInfo = jwt.verify(authToken, process.env.NEXT_PUBLIC_JWT_SECRET);
+
+        // Find all the rentals that belong to the user
+        const rentals = await prisma.rental.findMany({
+            where: {
+                renter: {
+                    id: parseInt(UserInfo.id),
+                },
+            },
+            include: {
+                item: true, // Include the item details
+            },
+        });
+
+        return NextResponse.json({ success: true, rentals }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        if (error instanceof jwt.JsonWebTokenError) {
             return NextResponse.json({ success: false, message: "JsonWebTokenError: invalid signature!" }, { status: 401 });
         }
         return NextResponse.json({ success: false, message: "Something went wrong!" }, { status: 500 });
