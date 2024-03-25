8e09fadc622791ac2500b04aef3437a4e146cad6
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2024-03-25 19:12:36 +0530

    Add toggle button to show only available items

--- app/items/page.js
+++ app/items/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { Switch } from '@mantine/core';
 import ItemCard from "../components/ItemCard/ItemCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Spinner from "../components/Spinner/Spinner";
@@ -11,6 +12,7 @@ const Items = () => {
   const [skip, setSkip] = useState(1);
   const [hasMoreData, setHasMoreData] = useState(true);
   const [shouldRunEffect, setShouldRunEffect] = useState(false);
+  const [showOnlyAvailable, setShowOnlyAvailable] = useState(true);
 
   useEffect(() => {
     if (!shouldRunEffect) {
@@ -37,24 +39,34 @@ const Items = () => {
 
   return (
     <div className="pt-8 padding-x">
+      <Switch
+        className="pt-10 px-2 flex justify-end"
+        checked={showOnlyAvailable}
+        onChange={(event) => setShowOnlyAvailable(event.currentTarget.checked)}
+        labelPosition="left"
+        color="yellow"
+        label="Show Only Available Items"
+      />
       <InfiniteScroll
         dataLength={items.length}
         next={fetchMoreData}
         hasMore={hasMoreData}
         loader={<Spinner />}
       >
-        <div className="grid 2xl:grid-cols-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-10">
-          {items?.map((item) => (
-            <div className="col-md-4" key={uuidv4()}>
-              <ItemCard
-                id={item.id}
-                title={item.title}
-                price={item.price}
-                description={item.description}
-                photos={item.photos[0]}
-              />
-            </div>
-          ))}
+        <div className="grid 2xl:grid-cols-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 py-3 px-2">
+          {items
+            .filter((item) => !showOnlyAvailable || item.isAvailable)
+            .map((item) => (
+              <div className="col-md-4" key={uuidv4()}>
+                <ItemCard
+                  id={item.id}
+                  title={item.title}
+                  price={item.price}
+                  description={item.description}
+                  photos={item.photos[0]}
+                />
+              </div>
+            ))}
         </div>
       </InfiniteScroll>
     </div>
